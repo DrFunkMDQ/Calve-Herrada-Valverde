eea1f9016e0500b998733f36da50aa3a843e11b5
juliancalve <julian.calve@gmail.com>
2018-10-28 01:12:27 -0300

    trabajado en lo de emi

--- STRANGER STRINGS 2/Peliculas.c
+++ STRANGER STRINGS 2/Peliculas.c
@@ -1,38 +1,40 @@
 #include "Peliculas.H"
 ///Struct Pelis
-void mostrarPelicula(stPelicula pelicula){
-printf("............\n\t-\t%d", pelicula.idPelicula);
-printf("\n\tNOMBRE:\t%s\n", pelicula.nombrePelicula);
-printf("\tDIRECTOR:\t%s\n", pelicula.director);
-printf("\tGENERO:\t%s\n", pelicula.genero);
-printf("\tPAIS:\t%s\n", pelicula.pais);
-printf("\tAÑO:\t%d\n", pelicula.anio);
-printf("\tVALORACION:\t%d\n", pelicula.valoracion);
-printf("\tPM:\t%d\n", pelicula.pm);
-if(pelicula.eliminado == 1)
-printf("ELIMINADO:\tSI\n");
-else
-    printf("ELIMINADO:\tNO\n");
+void mostrarPelicula(stPelicula pelicula)
+{
+    printf("............\n\t-\t%d", pelicula.idPelicula);
+    printf("\n\tNOMBRE:\t%s\n", pelicula.nombrePelicula);
+    printf("\tDIRECTOR:\t%s\n", pelicula.director);
+    printf("\tGENERO:\t%s\n", pelicula.genero);
+    printf("\tPAIS:\t%s\n", pelicula.pais);
+    printf("\tAÑO:\t%d\n", pelicula.anio);
+    printf("\tVALORACION:\t%d\n", pelicula.valoracion);
+    printf("\tPM:\t%d\n", pelicula.pm);
+    if(pelicula.eliminado == 1)
+        printf("ELIMINADO:\tSI\n");
+    else
+        printf("ELIMINADO:\tNO\n");
 }
 stPelicula crearPelicula()//En esto hay que agregar lo de meterse a buscar a ver si existe ya y lo del id autoincremental
 {
-stPelicula peliNueva;
-printf("Ingrese nombre de pelicula\n");
-scanf("%s", &peliNueva.nombrePelicula);
-printf("Ingrese director\n");
-scanf("%s", &peliNueva.director);
-printf("Ingrese genero\n");
-scanf("%s", &peliNueva.genero);
-printf("Ingrese pais\n");
-scanf("%s", &peliNueva.pais);
-printf("Ingrese año\n");
-scanf("%d", &peliNueva.anio);
-printf("Ingrese valoracion\n");
-scanf("%d", &peliNueva.valoracion);
-printf("Ingrese PM\n");
-scanf("%d", &peliNueva.pm);
-peliNueva.eliminado = 0;
-return peliNueva;}
+    stPelicula peliNueva;
+    printf("Ingrese nombre de pelicula\n");
+    scanf("%s", &peliNueva.nombrePelicula);
+    printf("Ingrese director\n");
+    scanf("%s", &peliNueva.director);
+    printf("Ingrese genero\n");
+    scanf("%s", &peliNueva.genero);
+    printf("Ingrese pais\n");
+    scanf("%s", &peliNueva.pais);
+    printf("Ingrese año\n");
+    scanf("%d", &peliNueva.anio);
+    printf("Ingrese valoracion\n");
+    scanf("%d", &peliNueva.valoracion);
+    printf("Ingrese PM\n");
+    scanf("%d", &peliNueva.pm);
+    peliNueva.eliminado = 0;
+    return peliNueva;
+}
 ///ARBOLES DE PELIS
 nodoArbol *inicArbol()
 {
@@ -69,3 +71,42 @@ void postorder(nodoArbol *arbol)
     }
 }
 
+nodoArbol *buscarPeliID(nodoArbol *arbol, int peliId)
+{
+    nodoArbol *aux = NULL;
+    if(arbol)
+    {
+        if(peliId == arbol->pelicula.idPelicula)
+        {
+            aux = arbol;
+        }
+        else if(peliId < arbol->pelicula.idPelicula)
+        {
+            aux = buscarPeliID(arbol->izq, peliId);
+        }
+        else
+        {
+            aux = buscarPeliID(arbol->der, peliId);
+        }
+    }
+    return aux;
+}
+
+nodoArbol *buscarPeliNombre(nodoArbol *arbol, char nombre[])
+{
+    nodoArbol *aux = NULL;
+    if(arbol)
+    {
+        if(strcmpi(arbol->pelicula.nombrePelicula, nombre) == 0)
+    {
+        aux = arbol;
+    }
+    else
+    {
+        aux = buscarPeliNombre(arbol->izq, nombre);
+            if(!aux)
+                aux = buscarPeliNombre(arbol->der, nombre);
+        }
+    }
+    return aux;
+}
