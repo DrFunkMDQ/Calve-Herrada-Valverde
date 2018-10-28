68412708e9154676e3dd07dc5f55a03cdc56a8f9
Martin Valverde <gmartinvalverde@gmail.com>
2018-10-27 22:25:19 -0300

    Creacion de structs

---
+++ STRANGER STRINGS 2/structs.h
@@ -0,0 +1,50 @@
+#include "string.h"
+#include "stdio.h"
+#include "stdlib.h"
+
+typedef struct
+{
+    int idPelicula;
+    char nombrePelicula[60];
+    char director[30];
+    char genero[20];
+    char pais[20];
+    int anio;
+    int valoracion;
+    int pm;
+    int eliminado;
+    char url[100];
+} stPelicula;
+
+typedef struct
+{
+    stPelicula pelicula;
+    struct nodoArbol *izq;
+    struct nodoArbol *der;
+} nodoArbol;
+
+typedef struct
+{
+    int idPeliVista;
+    int idUsuario;
+    int idPelicula;
+    int valUser;
+} stPelisVistas;
+
+typedef struct
+{
+    stUsuario usr;
+    nodoListaPelicula * listaPelis;
+    nodoUsuario * sig;
+} nodoUsuario;
+
+typedef struct{
+    int admin;
+    int idUsuario;
+    char nombreUsuario[15];
+    char genero;
+    char pais[20];
+    int pass [2][5];
+    int anioNacimiento;
+    int eliminado;
+}stUsuario;
